6bd47016d83b93321227ec4e16eeb0d68eea6635
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-08-30 13:56:53 +0200

    method to get input from user

--- ConsoleAppNewStart/Program.cs
+++ ConsoleAppNewStart/Program.cs
@@ -6,8 +6,37 @@ namespace ConsoleAppNewStart
     {
         static void Main(string[] args)
         {
+            /*
+             * block comment
+             * many lines
+             */ 
+             
             Console.WriteLine("Hello World!");
-            Console.WriteLine("First commit");
+            Console.WriteLine("I love to code");
+
+            string userAgeString = AskUserFor("your age");
+
+            Console.WriteLine("You typed in " + userAgeString + " as your age.");
+
+            int userAge = Convert.ToInt32(userAgeString);
+
+            if(userAge >= 18)
+            {
+                Console.WriteLine("Welcome to the bar and have a beer.");                
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the candy store, want a lollipop?");
+            }
+        }//end of main method
+                
+ //modifyer - return tyo - name - parameter inputs       
+        static string AskUserFor(string what)
+        {
+            Console.Write( "Please enter " + what + ": ");
+            string userInput = Console.ReadLine();
+
+            return userInput;
         }
-    }
-}
+    }//end of program class
+}//emd pf namespace

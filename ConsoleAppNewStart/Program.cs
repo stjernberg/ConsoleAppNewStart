dad629f46847de5cdc6dbd762781f5941cbaf421
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-08-30 10:35:43 +0200

    First commit

--- ConsoleAppNewStart/Program.cs
+++ ConsoleAppNewStart/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppNewStart
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First commit");
         }
     }
 }
